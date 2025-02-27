845b04e33c3fa4c1c44aff074ec0160ea8cedd84
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-02-26 22:01:12 -0300

    Adicionar arquivos de projeto.

---
+++ CRevisao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+namespace CRevisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que calcule a média do aluno
+
+            double nota1, nota2, media;
+            Console.Write("Digite a primeira nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1+nota2)/2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else if (media > 5)
+            {
+                Console.WriteLine("Recuperação.");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado.");
+            }
+            
+        }
+    }
+}
