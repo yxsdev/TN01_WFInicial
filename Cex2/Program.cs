b7fd989cddfc4b8272af102c20612f9128aecdc0
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-02-27 21:15:57 -0300

    feat: Ecercício 2 corrigido no console.

---
+++ Cex2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Cex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = @"
+----------------------------------
+1 - Domingo
+2 - Segunda-Feira
+3 - Terça-Feira
+4 - Quarta-Feira
+5 - Quinta-Feira
+6 - Sexta-Feira
+7 - Sábado
+----------------------------------";
+            
+            Console.WriteLine(menu);
+            byte opcao = byte.Parse(Console.ReadLine()!);
+
+            switch (opcao) 
+            { 
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda-Feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça-Feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta-Feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta-Feira");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta-Feira");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+    }
+}
