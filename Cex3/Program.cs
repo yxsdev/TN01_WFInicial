28f641c1c9b9021bce0851f05e57e2b182ad74e8
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-02-27 22:23:30 -0300

    feat: Exercício 3 do console.

---
+++ Cex3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Cex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 3 - Maior número
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("\nDigite o segundo: número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\nO maior número é: {maior}"); //Interpolação de string
+        }
+    }
+}
