eccbac60fa1592099e3474ce97cca75d38fd772e
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-02-28 22:20:48 -0300

    feat: Início do exercício 7 no windows forms.

--- WFInicial/FormEx7.Designer.cs
+++ WFInicial/FormEx7.Designer.cs
@@ -28,86 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblNome1 = new Label();
-            lblNome2 = new Label();
-            lblNome3 = new Label();
-            lblNome4 = new Label();
-            lblNome5 = new Label();
-            lblNome6 = new Label();
+            lblNome = new Label();
             txtNome1 = new TextBox();
-            txtNome2 = new TextBox();
-            txtNome3 = new TextBox();
-            txtNome4 = new TextBox();
-            txtNome5 = new TextBox();
-            txtNome6 = new TextBox();
             txtValor1 = new TextBox();
-            txtValor2 = new TextBox();
-            txtValor3 = new TextBox();
-            txtValor4 = new TextBox();
-            txtValor5 = new TextBox();
-            txtValor6 = new TextBox();
             lblValor1 = new Label();
-            lblValor2 = new Label();
-            lblValor3 = new Label();
-            lblValor4 = new Label();
-            lblValor5 = new Label();
-            lblValor6 = new Label();
             btnCalcular = new Button();
             SuspendLayout();
             // 
-            // lblNome1
+            // lblNome
             // 
-            lblNome1.AutoSize = true;
-            lblNome1.Location = new Point(82, 81);
-            lblNome1.Name = "lblNome1";
-            lblNome1.Size = new Size(98, 15);
-            lblNome1.TabIndex = 0;
-            lblNome1.Text = "Nome Produto 1:";
-            // 
-            // lblNome2
-            // 
-            lblNome2.AutoSize = true;
-            lblNome2.Location = new Point(82, 113);
-            lblNome2.Name = "lblNome2";
-            lblNome2.Size = new Size(98, 15);
-            lblNome2.TabIndex = 1;
-            lblNome2.Text = "Nome Produto 2:";
-            // 
-            // lblNome3
-            // 
-            lblNome3.AutoSize = true;
-            lblNome3.Location = new Point(82, 148);
-            lblNome3.Name = "lblNome3";
-            lblNome3.Size = new Size(98, 15);
-            lblNome3.TabIndex = 2;
-            lblNome3.Text = "Nome Produto 3:";
-            // 
-            // lblNome4
-            // 
-            lblNome4.AutoSize = true;
-            lblNome4.Location = new Point(82, 181);
-            lblNome4.Name = "lblNome4";
-            lblNome4.Size = new Size(98, 15);
-            lblNome4.TabIndex = 3;
-            lblNome4.Text = "Nome Produto 4:";
-            // 
-            // lblNome5
-            // 
-            lblNome5.AutoSize = true;
-            lblNome5.Location = new Point(82, 215);
-            lblNome5.Name = "lblNome5";
-            lblNome5.Size = new Size(98, 15);
-            lblNome5.TabIndex = 4;
-            lblNome5.Text = "Nome Produto 5:";
-            // 
-            // lblNome6
-            // 
-            lblNome6.AutoSize = true;
-            lblNome6.Location = new Point(82, 248);
-            lblNome6.Name = "lblNome6";
-            lblNome6.Size = new Size(98, 15);
-            lblNome6.TabIndex = 5;
-            lblNome6.Text = "Nome Produto 6:";
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(82, 81);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(89, 15);
+            lblNome.TabIndex = 0;
+            lblNome.Text = "Nome Produto:";
+            lblNome.Click += lblNome1_Click;
             // 
             // txtNome1
             // 
@@ -116,208 +52,55 @@
             txtNome1.Size = new Size(113, 23);
             txtNome1.TabIndex = 6;
             // 
-            // txtNome2
-            // 
-            txtNome2.Location = new Point(195, 110);
-            txtNome2.Name = "txtNome2";
-            txtNome2.Size = new Size(113, 23);
-            txtNome2.TabIndex = 7;
-            // 
-            // txtNome3
-            // 
-            txtNome3.Location = new Point(195, 145);
-            txtNome3.Name = "txtNome3";
-            txtNome3.Size = new Size(113, 23);
-            txtNome3.TabIndex = 8;
-            // 
-            // txtNome4
-            // 
-            txtNome4.Location = new Point(195, 178);
-            txtNome4.Name = "txtNome4";
-            txtNome4.Size = new Size(113, 23);
-            txtNome4.TabIndex = 9;
-            // 
-            // txtNome5
-            // 
-            txtNome5.Location = new Point(195, 212);
-            txtNome5.Name = "txtNome5";
-            txtNome5.Size = new Size(113, 23);
-            txtNome5.TabIndex = 10;
-            // 
-            // txtNome6
-            // 
-            txtNome6.Location = new Point(195, 245);
-            txtNome6.Name = "txtNome6";
-            txtNome6.Size = new Size(113, 23);
-            txtNome6.TabIndex = 11;
-            // 
             // txtValor1
             // 
-            txtValor1.Location = new Point(419, 78);
+            txtValor1.Location = new Point(195, 107);
             txtValor1.Name = "txtValor1";
             txtValor1.Size = new Size(113, 23);
             txtValor1.TabIndex = 12;
             // 
-            // txtValor2
-            // 
-            txtValor2.Location = new Point(419, 109);
-            txtValor2.Name = "txtValor2";
-            txtValor2.Size = new Size(113, 23);
-            txtValor2.TabIndex = 13;
-            // 
-            // txtValor3
-            // 
-            txtValor3.Location = new Point(419, 144);
-            txtValor3.Name = "txtValor3";
-            txtValor3.Size = new Size(113, 23);
-            txtValor3.TabIndex = 14;
-            // 
-            // txtValor4
-            // 
-            txtValor4.Location = new Point(419, 178);
-            txtValor4.Name = "txtValor4";
-            txtValor4.Size = new Size(113, 23);
-            txtValor4.TabIndex = 15;
-            // 
-            // txtValor5
-            // 
-            txtValor5.Location = new Point(419, 212);
-            txtValor5.Name = "txtValor5";
-            txtValor5.Size = new Size(113, 23);
-            txtValor5.TabIndex = 16;
-            // 
-            // txtValor6
-            // 
-            txtValor6.Location = new Point(419, 245);
-            txtValor6.Name = "txtValor6";
-            txtValor6.Size = new Size(113, 23);
-            txtValor6.TabIndex = 17;
-            // 
             // lblValor1
             // 
             lblValor1.AutoSize = true;
-            lblValor1.Location = new Point(377, 81);
+            lblValor1.Location = new Point(82, 107);
             lblValor1.Name = "lblValor1";
             lblValor1.Size = new Size(36, 15);
             lblValor1.TabIndex = 18;
             lblValor1.Text = "Valor:";
             // 
-            // lblValor2
-            // 
-            lblValor2.AutoSize = true;
-            lblValor2.Location = new Point(377, 113);
-            lblValor2.Name = "lblValor2";
-            lblValor2.Size = new Size(36, 15);
-            lblValor2.TabIndex = 19;
-            lblValor2.Text = "Valor:";
-            // 
-            // lblValor3
-            // 
-            lblValor3.AutoSize = true;
-            lblValor3.Location = new Point(377, 148);
-            lblValor3.Name = "lblValor3";
-            lblValor3.Size = new Size(36, 15);
-            lblValor3.TabIndex = 20;
-            lblValor3.Text = "Valor:";
-            // 
-            // lblValor4
-            // 
-            lblValor4.AutoSize = true;
-            lblValor4.Location = new Point(377, 181);
-            lblValor4.Name = "lblValor4";
-            lblValor4.Size = new Size(36, 15);
-            lblValor4.TabIndex = 21;
-            lblValor4.Text = "Valor:";
-            // 
-            // lblValor5
-            // 
-            lblValor5.AutoSize = true;
-            lblValor5.Location = new Point(377, 215);
-            lblValor5.Name = "lblValor5";
-            lblValor5.Size = new Size(36, 15);
-            lblValor5.TabIndex = 22;
-            lblValor5.Text = "Valor:";
-            // 
-            // lblValor6
-            // 
-            lblValor6.AutoSize = true;
-            lblValor6.Location = new Point(377, 248);
-            lblValor6.Name = "lblValor6";
-            lblValor6.Size = new Size(36, 15);
-            lblValor6.TabIndex = 23;
-            lblValor6.Text = "Valor:";
-            // 
             // btnCalcular
             // 
-            btnCalcular.Location = new Point(419, 318);
+            btnCalcular.Location = new Point(195, 155);
             btnCalcular.Name = "btnCalcular";
             btnCalcular.Size = new Size(113, 23);
             btnCalcular.TabIndex = 24;
             btnCalcular.Text = "Calcular";
             btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
             // 
             // FormEx7
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(657, 410);
+            ClientSize = new Size(445, 275);
             Controls.Add(btnCalcular);
-            Controls.Add(lblValor6);
-            Controls.Add(lblValor5);
-            Controls.Add(lblValor4);
-            Controls.Add(lblValor3);
-            Controls.Add(lblValor2);
             Controls.Add(lblValor1);
-            Controls.Add(txtValor6);
-            Controls.Add(txtValor5);
-            Controls.Add(txtValor4);
-            Controls.Add(txtValor3);
-            Controls.Add(txtValor2);
             Controls.Add(txtValor1);
-            Controls.Add(txtNome6);
-            Controls.Add(txtNome5);
-            Controls.Add(txtNome4);
-            Controls.Add(txtNome3);
-            Controls.Add(txtNome2);
             Controls.Add(txtNome1);
-            Controls.Add(lblNome6);
-            Controls.Add(lblNome5);
-            Controls.Add(lblNome4);
-            Controls.Add(lblNome3);
-            Controls.Add(lblNome2);
-            Controls.Add(lblNome1);
+            Controls.Add(lblNome);
             Name = "FormEx7";
             Text = "FormEx7";
+            Load += FormEx7_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label lblNome1;
-        private Label lblNome2;
-        private Label lblNome3;
-        private Label lblNome4;
-        private Label lblNome5;
-        private Label lblNome6;
+        private Label lblNome;
         private TextBox txtNome1;
-        private TextBox txtNome2;
-        private TextBox txtNome3;
-        private TextBox txtNome4;
-        private TextBox txtNome5;
-        private TextBox txtNome6;
         private TextBox txtValor1;
-        private TextBox txtValor2;
-        private TextBox txtValor3;
-        private TextBox txtValor4;
-        private TextBox txtValor5;
-        private TextBox txtValor6;
         private Label lblValor1;
-        private Label lblValor2;
-        private Label lblValor3;
-        private Label lblValor4;
-        private Label lblValor5;
-        private Label lblValor6;
         private Button btnCalcular;
     }
 }
--- WFInicial/FormEx7.cs
+++ WFInicial/FormEx7.cs
@@ -12,9 +12,28 @@ namespace WFInicial
 {
     public partial class FormEx7 : Form
     {
+        public string[] NomeProdutos = new string[6];
+        public double[] PrecoProdutos = new double[6];
+
         public FormEx7()
         {
             InitializeComponent();
+        }
+
+        private void lblNome1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormEx7_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+           int quantVetor = NomeProdutos.Length;
+           
         }
     }
 }
